b63275a2e6863bf76eac32125135ab2efa689645
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2022-01-10 12:17:49 -0500

    Setup test and fix coverage scripts.

---
+++ test/mocha/01-api.js
@@ -0,0 +1,31 @@
+/*!
+ * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const {documentLoader} = require('bedrock-jsonld-document-loader');
+
+describe('bedrock-security-context', () => {
+  it('sets up contexts properly', async () => {
+    const testContextModules = [
+      'did-context',
+      'veres-one-context',
+    ];
+    for(const module of testContextModules) {
+      const {contexts, constants: contextConstants} = require(module);
+
+      for(const c in contextConstants) {
+        if(!c.includes('URL')) {
+          continue;
+        }
+
+        // ensure that context documents are defined
+        const result = await documentLoader(contextConstants[c]);
+        should.exist(result);
+        should.exist(result.document);
+        result.document.should.be.an('object');
+        result.document.should.eql(contexts.get(contextConstants[c]));
+      }
+    }
+  });
+});
---
+++ test/test.js
@@ -0,0 +1,10 @@
+/*!
+ * Copyright (c) 2018-2022 Digital Bazaar, Inc. All rights reserved.
+ */
+'use strict';
+
+const bedrock = require('bedrock');
+require('bedrock-veres-one-context');
+
+require('bedrock-test');
+bedrock.start();
